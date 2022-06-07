7aabaa1c028c084ffc0f0c1ab96a9b7e06a294cf
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-07 12:59:46 +0200

    $0012: [Interface] [Proposal]: add navigation link to home + proposal details section: description

--- src/pages/Proposal.js
+++ src/pages/Proposal.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./pages.css";
 
+import { Tag, Widget, Blockie, Tooltip, Icon, Form, Table } from "web3uikit";
+import { Link } from "react-router-dom"; // to route back to home page
+
 const Proposal = () => {
 
   return (
     <>
       <div className="contentProposal">
-     </div>
+        <div className="proposal">
+          <Link to="/">
+            <div className="backHome">
+              <Icon fill="#ffffff" size={20} svg="chevronLeft" />
+              Overview
+            </div>
+          </Link>
+          <div>g3p for web3 moralis king pin of the month?</div>
+        </div>
+      </div>
     </>
   );
 };
