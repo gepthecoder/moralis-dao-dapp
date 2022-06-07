2457415f251c58302e79640464752c3265a5efd2
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-07 13:02:44 +0200

    $0013: [Interface] [Proposal]: proposal overview with tool tip of proposer

--- src/pages/Proposal.js
+++ src/pages/Proposal.js
@@ -16,7 +16,18 @@ const Proposal = () => {
               Overview
             </div>
           </Link>
+          
           <div>g3p for web3 moralis king pin of the month?</div>
+
+          <div className="proposalOverview">
+            <Tag color={"red"} text={"Rejected"} />
+            <div className="proposer">
+              <span>Proposed By </span>
+              <Tooltip content={"0xe276941FBd5f936E677dB9B6eEE8212a3b268C5E"}>
+                <Blockie seed={"0xe276941FBd5f936E677dB9B6eEE8212a3b268C5E"} />
+              </Tooltip>
+            </div>
+          </div>
         </div>
       </div>
     </>
