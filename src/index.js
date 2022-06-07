24252f627c80915f297db20bc91b731da54fad07
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-07 11:47:19 +0200

    $0005: [MoralisSetup] [Interface]: add moralis appid and server url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="xxx" serverUrl="xxx">
+    <MoralisProvider appId={process.env.REACT_APP_APP_ID} serverUrl={process.env.REACT_APP_SERVER_URL}>
       <BrowserRouter>
           <App />
       </BrowserRouter>
