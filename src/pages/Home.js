b24eb697126f76e2061c7442ff9b103052bb6cb1
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-07 12:08:12 +0200

    $0007: [Interface] [Home]: add navigation content (DAO, Forum, Docs)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./pages.css";
 
+import { TabList, Tab, Widget, Tag, Table, Form } from "web3uikit";
+
+
 
 const Home = () => {
  
@@ -12,6 +15,12 @@ const Home = () => {
   return (
     <>
       <div className="content">
+        <TabList defaultActiveKey={1} tabStyle="bulbUnion">
+          <Tab tabKey={1} tabName="DAO">
+          </Tab>
+          <Tab tabKey={2} tabName="Forum"></Tab>
+          <Tab tabKey={3} tabName="Docs"></Tab>
+        </TabList>
       </div>
     </>
   );
