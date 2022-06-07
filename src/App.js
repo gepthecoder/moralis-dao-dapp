5d9a3411b41294dedafa67d5128c033d3f01813e
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-07 12:04:33 +0200

    $0006: [Interface] [Header]: connect button and cyber jawz dao logo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Proposal from "./pages/Proposal";
 
+import daoLogo from "./images/dao-logo.png";
+import { ConnectButton } from "web3uikit";
+
 const App = () => {
   return (
     <>
+      <div className="header">
+        <img width="160px" src={daoLogo} alt="logo" />
+        <ConnectButton />
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/proposal" element={<Proposal />} />
